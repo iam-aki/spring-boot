febb865cecc850dc09f79b4724c1a8608e602630
Ankit Aggarwal <17708554+iam-aki@users.noreply.github.com>
2020-04-21 23:38:27 +0530

    Spring Security Basic Implementation
    Basic implementation of Spring Security

---
+++ spring-security/src/main/java/com/iamaki/springboot/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.iamaki.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  @GetMapping("/login")
+  public String login() {
+    return "Hello Logi";
+  }
+
+  @GetMapping("/show")
+  public String show() {
+    return "Hello Logi";
+  }
+}
---
+++ spring-security/src/main/java/com/iamaki/springboot/security/CustomUserDetailService.java
@@ -0,0 +1,16 @@
+package com.iamaki.springboot.security;
+
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailService implements UserDetailsService {
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return new User("admin", "admin", new ArrayList<>());
+  }
+}
