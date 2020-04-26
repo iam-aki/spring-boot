8e5f91d26cf0cb68eb5f23057a6e08d4ba1f7943
Ankit Aggarwal <17708554+iam-aki@users.noreply.github.com>
2020-04-26 13:12:17 +0530

    Mongo Session Enabled.

--- spring-security-session-form/src/main/java/com/iamaki/springboot/Application.java
+++ spring-security-session-form/src/main/java/com/iamaki/springboot/Application.java
@@ -2,8 +2,10 @@ package com.iamaki.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
 @SpringBootApplication
+@EnableMongoHttpSession
 public class Application {
 
   public static void main(String[] args) {
